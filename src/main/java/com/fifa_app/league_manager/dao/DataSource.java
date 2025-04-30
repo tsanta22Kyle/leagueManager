518b5d32c92fb5a23442641cdc041027faf3b317
dieudonne19 <hei.dieudonne.19@gmail.com>
2025-04-30 11:17:06 +0300

    feat: implement DataSource class

--- src/main/java/com/fifa_app/league_manager/dao/DataSource.java
+++ src/main/java/com/fifa_app/league_manager/dao/DataSource.java
@@ -1,4 +1,30 @@
 package com.fifa_app.league_manager.dao;
 
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+@Configuration
 public class DataSource {
+    private final static int defaultPort = 5432;
+    private final String host = System.getenv("DATABASE_HOST");
+    private final String user = System.getenv("DATABASE_USER");
+    private final String password = System.getenv("DATABASE_PASSWORD");
+    private final String database = System.getenv("DATABASE_NAME");
+    private final String jdbcUrl;
+
+    public DataSource() {
+        jdbcUrl = "jdbc:postgresql://" + host + ":" + defaultPort + "/" + database;
+    }
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(jdbcUrl, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
