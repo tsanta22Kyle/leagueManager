8ee3a96c4f09a24c8b2e188a134e1a529c981430
dieudonne19 <hei.dieudonne.19@gmail.com>
2025-05-10 13:22:09 +0300

    feat: implement playerSeason getByPrimaryKey, getAll

--- src/main/java/com/fifa_app/league_manager/dao/operations/PlayerSeasonCrudOperation.java
+++ src/main/java/com/fifa_app/league_manager/dao/operations/PlayerSeasonCrudOperation.java
@@ -1,22 +1,114 @@
 package com.fifa_app.league_manager.dao.operations;
 
 
+import com.fifa_app.league_manager.model.Club;
+import com.fifa_app.league_manager.model.Coach;
 import com.fifa_app.league_manager.model.PlayerStatistics;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-@Repository@RequiredArgsConstructor
-public class PlayerSeasonCrudOperation {
+@Repository
+@RequiredArgsConstructor
+public class PlayerSeasonCrudOperation implements CrudOperations<PlayerStatistics> {
 
 
+    public List<PlayerStatistics> saveAll(List<PlayerStatistics> playerStatisticsList) {
 
+    }
 
-        public List<PlayerStatistics> saveAll(List<PlayerStatistics> playerStatisticsList) {
-
+    @Override
+    @SneakyThrows
+    public List<PlayerStatistics> getAll() {
+        List<PlayerStatistics> stats = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("select id, player_id, season_id, total_playing_time, scored_goals" +
+                     " from player_season order by id;")) {
+            /*
+            statement.setInt(1, pageSize);
+            statement.setInt(2, pageSize * (page - 1));
+             */
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    PlayerStatistics playerStatistics = playerSeasonMapper.apply(resultSet);
+                    stats.add(playerStatistics);
+                }
+            }
+            return stats;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+    }
 
+    @SneakyThrows
+    public PlayerStatistics getByPlayerIdAndSeasonId(String playerId, String seasonId) {
+        PlayerStatistics stat = null;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("select id, player_id, season_id, total_playing_time, scored_goals" +
+                     " from player_season where player_id = ? and season_id = ? order by id;")) {
+            statement.setString(1, playerId);
+            statement.setString(2, seasonId);
+            /*
+             */
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    stat = playerSeasonMapper.apply(resultSet);
+                }
+            }
+            return stat;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    @SneakyThrows
+    public List<PlayerStatistics> getBySeasonId(String seasonId) {
+        List<PlayerStatistics> stats = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("select id, player_id, season_id, total_playing_time, scored_goals" +
+                     " from player_season where season_id = ? order by id;")) {
+            statement.setString(1, seasonId);
+            /*
+            statement.setInt(2, pageSize * (page - 1));
+             */
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    PlayerStatistics playerStatistics = playerSeasonMapper.apply(resultSet);
+                    stats.add(playerStatistics);
+                }
+            }
+            return stats;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    @SneakyThrows
+    public List<PlayerStatistics> getByPlayerId(String playerId) {
+        List<PlayerStatistics> stats = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("select id, player_id, season_id, total_playing_time, scored_goals" +
+                     " from player_season where player_id = ? order by id;")) {
+            statement.setString(1, playerId);
+            /*
+            statement.setInt(2, pageSize * (page - 1));
+             */
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    PlayerStatistics playerStatistics = playerSeasonMapper.apply(resultSet);
+                    stats.add(playerStatistics);
+                }
+            }
+            return stats;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
